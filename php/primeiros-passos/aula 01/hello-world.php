99f15b68c441f7e5ea0c40bf874dea5187e47d69
Lucas W. da Motta <lucaswantz@hotmail.com>
2020-09-10 08:41:04 -0300

    Testes

--- php/primeiros-passos/aula 01/hello-world.php
+++ php/primeiros-passos/aula 01/hello-world.php
@@ -1,3 +1,13 @@
 <?php
 
 echo "hello world";
+
+$codigoPromocional = "aaaads";
+
+if (!$codigoPromocional) {
+	echo "Falso";
+}
+
+$teste = strcasecmp(substr($codigoPromocional, -3), "ads");
+
+echo $teste;
