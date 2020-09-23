6dc6b247c8a15b767550d7d8539597bb95f36184
lucaswantz <lucaswantz@hotmail.com>
2020-09-23 14:42:10 -0300

    Alura

--- javascript/js/principal.js
+++ javascript/js/principal.js
@@ -1,32 +1,40 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+for (let i = 0; i < pacientes.length; i++) {
+    var paciente = pacientes[i];
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
 
-var tdImc = paciente.querySelector(".info-imc");
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
 
-var pesoEhValido = true;
-var alturaEhValido = true;
+    var tdImc = paciente.querySelector(".info-imc");
 
-if (peso <= 0 || peso >= 1000) {
-    tdImc.textContent = "Peso inválido";
-    pesoEhValido = false;
+    var pesoEhValido = true;
+    var alturaEhValido = true;
+
+    if (peso <= 0 || peso >= 1000) {
+        tdImc.textContent = "Peso inválido";
+        pesoEhValido = false;
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (altura <= 0 || altura >= 3) {
+        tdImc.textContent = "Altura inválido";
+        alturaEhValido = false;
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (pesoEhValido && alturaEhValido) {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    } else {
+        tdImc.textContent = "Altura e/ou peso inválidos!";
+    }
 }
 
-if (altura <= 0 || altura >= 3) {
-    tdImc.textContent = "Altura inválido";
-    alturaEhValido = false;
-}
 
-if (pesoEhValido && alturaEhValido) {
-    var imc = peso / (altura * altura);
-    tdImc.textContent = imc;
-} else {
-    tdImc.textContent = "Altura e/ou peso inválidos!";
-}
