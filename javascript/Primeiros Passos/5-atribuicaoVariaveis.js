e4a7ac0e8f13d01835531463d4c461990dec281e
lucaswantz <lucas.motta@tiny.com.br>
2020-12-08 16:54:22 -0300

    Alura: javascript

---
+++ javascript/Primeiros Passos/5-atribuicaoVariaveis.js
@@ -0,0 +1,11 @@
+console.log("Trabalhanco com atribuição de variáveis");
+
+const idade = 29;
+const primeiroNome = "Lucas";
+const sobrenome = "Motta";
+
+console.log(primeiroNome, sobrenome);
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`);
+
+const nomeCompleto = primeiroNome + sobrenome;
+console.log(nomeCompleto);
