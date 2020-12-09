678eda61351c047a3835ff8f9f2804b83399c8d5
lucaswantz <lucas.motta@tiny.com.br>
2020-12-09 11:41:02 -0300

    Alura: javascript

--- javascript/Primeiros Passos/7-condicionais.js
+++ javascript/Primeiros Passos/7-condicionais.js
@@ -8,18 +8,34 @@ const listaDeDestino = new Array(
 
 const idadeComprador = 15;
 const estaAcompanhada = true;
+const temPassagemComprada = true;
 
 console.log("Destinos Possíveis");
 console.log(listaDeDestino);
 
-if (idadeComprador >= 18) {
-    console.log("Comprador é maior de idade");
-    listaDeDestino.splice(1, 1); // Removendo item
-} else if (estaAcompanhada) {
-    console.log("Comprador está acompanhado");
+// if (idadeComprador >= 18) {
+//     console.log("Comprador é maior de idade");
+//     listaDeDestino.splice(1, 1); // Removendo item
+// } else if (estaAcompanhada) {
+//     console.log("Comprador está acompanhado");
+//     listaDeDestino.splice(1, 1); // Removendo item
+// } else {
+//     console.log("Não é maior de idade e não posso vender");
+// }
+
+if (idadeComprador >= 18 || estaAcompanhada) {
+    console.log("Boa Viagem!");
     listaDeDestino.splice(1, 1); // Removendo item
 } else {
     console.log("Não é maior de idade e não posso vender");
+}
+
+console.log("\nEmbarque:\n");
+
+if (idadeComprador >= 18 && temPassagemComprada) {
+    console.log("Boa Viagem!");
+} else {
+    console.log("Você não pode embarcar");
 }
 
 console.log(listaDeDestino);
