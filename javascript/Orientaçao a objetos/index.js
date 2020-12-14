f81c9d6a5f6c2ef775e321168eea876959887c8f
lucaswantz <lucas.motta@tiny.com.br>
2020-12-14 13:55:30 -0300

    Alura: javascript OO

--- javascript/Orientaçao a objetos/index.js
+++ javascript/Orientaçao a objetos/index.js
@@ -1,18 +1,5 @@
-class Cliente {
-    nome;
-    cpj;
-}
-
-class ContaCorrente {
-    agencia;
-    saldo;
-
-    sacar(valor) {
-        if (this.saldo >= valor) {
-            this.saldo -= valor;
-        }
-    }
-}
+import { Cliente } from './Cliente.js';
+import { ContaCorrente } from './ContaCorrente.js';
 
 const cliente1 = new Cliente();
 cliente1.nome = "Lucas Motta";
@@ -20,9 +7,12 @@ cliente1.cpj = "008.444.140-26";
 
 const contaCorrenteLucas = new ContaCorrente();
 contaCorrenteLucas.agencia = "453";
-contaCorrenteLucas.saldo = 100;
 
-contaCorrenteLucas.sacar(200);
+contaCorrenteLucas.depositar(-100);
+contaCorrenteLucas.depositar(100);
+contaCorrenteLucas.depositar(100);
 
-console.log(cliente1);
+const valorSacado = contaCorrenteLucas.sacar(50);
+
+console.log(valorSacado);
 console.log(contaCorrenteLucas);
