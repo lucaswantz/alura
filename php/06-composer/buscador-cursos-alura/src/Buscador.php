acaa3bf0ec9e5bd1b0eeab89df1108f1f7aca79a
Lucas Wantz da Motta <lucaswantz@hotmail.com>
2021-06-03 15:02:28 -0300

    Composer

---
+++ php/06-composer/buscador-cursos-alura/src/Buscador.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Alura\BuscadorDeCursos;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\ClientInterface;
+use Symfony\Component\DomCrawler\Crawler;
+
+class Buscador {
+
+	private $httpClient;
+	private $DOMCrawler;
+
+	public function __construct(ClientInterface $httpClient, Crawler $DOMCrawler) {
+		$this->httpClient = $httpClient;
+		$this->DOMCrawler = $DOMCrawler;
+	}
+
+	public function buscar(string $url) : array {
+		$resposta = $this->httpClient->request("GET", $url);
+
+		$html = $resposta->getBody();
+ 		$this->DOMCrawler->addHtmlContent($html);
+
+		$elementosCursos = $this->DOMCrawler->filter("span.card-curso__nome");
+		$cursos = [];
+
+		foreach ($elementosCursos as $elemento) {
+			$cursos[] = $elemento->textContent;
+		}
+
+		return $cursos;
+	}
+}
