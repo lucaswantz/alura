373f076ff53d50306af07734551c36edd665c677
Lucas Wantz da Motta <lucaswantz@hotmail.com>
2021-06-08 08:05:52 -0300

    Listar alunos

--- php/07-PDO-trabalhando-com-banco-dados/lista-alunos.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once('vendor/autoload.php');
-
-$databasePath = __DIR__ . '/banco.sqlite';
-$pdo = new PDO('sqlite:' . $databasePath);
-
-$statement = $pdo->query('SELECT * FROM students');
-$studentList = $statement->fetchAll();
