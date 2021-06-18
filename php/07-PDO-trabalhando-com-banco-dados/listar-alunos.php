185246ff2520b3ba6cb29d5fa2832997a5b74f82
lucaswantz <lucas.motta@tiny.com.br>
2021-06-18 17:20:58 -0300

    PDO: Repository

--- php/07-PDO-trabalhando-com-banco-dados/listar-alunos.php
+++ php/07-PDO-trabalhando-com-banco-dados/listar-alunos.php
@@ -21,6 +21,6 @@ var_dump($studentList);
 
 // while ($studentData = $statement->fetch(PDO::FETCH_ASSOC)) {
 // 	$student = new Student($studentData["id"], $studentData["name"], new DateTimeImmutable($studentData["birth_date"]));
-	
+
 // 	echo $student->age() . PHP_EOL;
 // }
