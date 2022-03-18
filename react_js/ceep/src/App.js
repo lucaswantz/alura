51493e1247a88c8f75654d2e8a486ea6e87380f2
Lucas Wantz da Motta <lucaswantz@hotmail.com>
2022-03-18 14:05:00 -0300

    Alura: Inicio curso de React

---
+++ react_js/ceep/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <form>
+      <input type="text" placeholder='Título'/>
+      <textarea placeholder='Escreva sua nota'/>
+    </form>
+  );
+}
+
+export default App;
